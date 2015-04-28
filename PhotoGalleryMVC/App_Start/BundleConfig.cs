b0753c93df75d1e14f7d46190ab058c413f9bfa3
nroman <nebeliuk.roman@gmail.com>
2015-04-27 23:40:28 -0400

    site template
    just a new project with the basic config

---
+++ PhotoGalleryMVC/App_Start/BundleConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace PhotoGalleryMVC.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Content/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/Site.css"));
+        }
+    }
+}
